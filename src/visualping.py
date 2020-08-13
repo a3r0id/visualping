8513f3bd28ffbfa0297e8bb10500ac32975ce42c
aerobotpro <50676042+aerobotpro@users.noreply.github.com>
2020-08-12 21:42:45 -0700

    Update visualping.py

--- src/visualping.py
+++ src/visualping.py
@@ -3,7 +3,7 @@
 """
 LICENSE
 
-Copyright 2020 [Aerobot Development]
+Copyright 2020 [Aerobot Labs]
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
 to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
